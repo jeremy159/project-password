084310fc7a5b09395a1e8b3801fba1cc018891a6
Maxime Thibault <maxx_thibault@hotmail.com>
2019-03-25 11:19:56 -0400

    ...

--- nb_password/assets/scripts/main.js
+++ nb_password/assets/scripts/main.js
@@ -17,7 +17,6 @@
     for(var i=0; i < 60; i++) {
         times.push(i);
     }
-    var datasets = ["data/crackingTimes.csv"];
 
     var svg = d3.select("#heatmap").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -52,87 +51,118 @@
         .attr("transform", `translate(${gridSize / 2}, -6)`)
         .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
-    var heatmap = function(file) {
-        return new Promise((resolve) => {
-            d3.csv(file).then(function(data) {
-                data = data.slice(0,60);
-                var max = d3.max(data, d => parseInt(d.t));
-                var colorScale = d3.scaleQuantile()
-                    .domain([0, buckets - 1, max])
-                    .range(colors);
-
-                var cards = svg.selectAll(".hour")
-                    .data(data, function(d) {
-                        return parseInt(d.t);
-                    });
-
-                //cards.append("title");
-
-                cards.enter().append("rect")
-                    .attr("x", function(d, i) { 
-                        return i * gridSize; 
-                    })
-                    .attr("y", function(d, i) { 
-                        //return i * gridSize;
-                        return 0;
-                    })
-                    //.attr("rx", 4)
-                    //.attr("ry", 4)
-                    .attr("class", "hour bordered")
-                    .attr("width", gridSize)
-                    .attr("height", gridSize*3)
-                    .transition().duration(1000)
-                    .style("fill", d => colorScale(parseInt(d.t)));
-                
-                cards.exit().remove();
-
-               var legend = svg.selectAll(".legend")
-                    .data([0].concat(colorScale.quantiles()), function(d) { 
-                        return d; 
-                    });
-
-                legend.enter().append("g")
-                    .attr("class", "legend")
-                    .append("rect")
-                    .attr("x", function(d, i) { 
-                        return legendElementWidth * i; 
-                    })
-                    .attr("y", height)
-                    .attr("width", legendElementWidth)
-                    .attr("height", gridSize)
-                    .style("fill", function(d, i) { 
-                        return colors[i]; 
-                    });
-
-                legend.enter().append("text")
-                    .attr("class", "mono")
-                    .text(function(d) { 
-                        return "≥ " + Math.round(d); 
-                    })
-                    .style("text-anchor", "start")
-                    .attr("x", function(d, i) { 
-                        return legendElementWidth * i; 
-                    })
-                    .attr("y", height + gridSize*2)
-
-                legend.exit().remove();
-                resolve("Done!");
-            });
+        var json = {}
+        await new Promise((resolve) => {
+            d3.json('data.json').then(function(data) {
+                json = data;
+                resolve('done!');
+            })
         });
-    }
-
-    await heatmap(datasets[0]);
     
-    /*var datasetpicker = d3.select("#dataset-picker").selectAll(".dataset-button")
-        .data(datasets);
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([height + 10, 0])
+        .html(d => format(d.value));
+
+    var count = d3.select("body")
+        .append("div");
+
+    var heatmap = function(data) {        
+        // Log Scale
+        
+        var c = 0;
+        var domain = [json.minimum, json.maximum]
+        const logScale = d3.scaleLog().domain(domain);
+        const colorScale = d3.scaleSequential(d => {
+            var log = logScale(d);
+            return d3.interpolateOrRd(log);
+        });
+
+        //Suppression des rectangles
+        svg.selectAll(".bordered").remove();
+
+        // update heatmap 1D
+        var cards = svg.selectAll(".bar").data(data);
+        // cards.append("title");
+        cards.enter().append("rect")
+            .attr("x", function(d, i) { 
+                return i * gridSize; 
+            })
+            .attr("y", function(d, i) { 
+                //return i * gridSize;
+                return 0;
+            })
+            //.attr("rx", 4)
+            //.attr("ry", 4)
+            .attr("class", "bordered")
+            .attr("width", gridSize)
+            .attr("height", gridSize*3) 
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+            .style("fill", colorScale(1))
+            .transition().duration((d, i) => 300)
+            .delay((d,i) => {
+                var delay = i*100;
+                setTimeout(() => {
+                    c += d.value;
+                    count.html(format(c) + " mots de passes décryptés");
+                }, delay);
+                return delay;
+            })
+            .style("fill", d => {
+                return colorScale(d.value)
+            })
+        svg.call(tip);
+
+        //Update legends
+       // https://bl.ocks.org/RobinL/6b27f9abc591002779d294f1fdff6b72
+        /*svg.selectAll(".legend").remove();
+        var legend = svg.selectAll(".legend")
+            .data([0].concat(colorScale.quantiles()), function(d) { 
+                return d; 
+            });
+
+        legend.enter().append("g")
+            .attr("class", "legend")
+            .append("rect")
+            .attr("x", function(d, i) { 
+                return legendElementWidth * i; 
+            })
+            .attr("y", height)
+            .attr("width", legendElementWidth)
+            .attr("height", gridSize)
+            .style("fill", function(d, i) { 
+                return colors[i]; 
+            });
+
+        svg.selectAll('.tmp').remove();
+        legend.enter().append("text")
+            .attr("class", "mono tmp")
+            .text(function(d) { 
+                return "≥ " + format(Math.round(d)); 
+            })
+            .style("text-anchor", "start")
+            .attr("x", function(d, i) { 
+                return legendElementWidth * i; 
+            })
+            .attr("y", height + gridSize*2)
+
+        legend.exit().remove();*/
+    };
+
+    
+    heatmap(json.data['seconds']);
+
+    var datasets = ['seconds', 'minutes', 'hours', 'days', 'months', 'years'];
+    var datasetpicker = d3.select("#dataset-picker")
+                            .selectAll(".dataset-button")
+                            .data(datasets);
 
     datasetpicker.enter()
         .append("input")
-        .attr("value", function(d){ return "Dataset " + d })
+        .attr("value", d => d)
         .attr("type", "button")
         .attr("class", "dataset-button")
-        .on("click", function(d) {
-            heatmapChart(d);
-        });*/
+        .on("click", d => heatmap(json.data[d]));
 
 })(d3, localization);
---
+++ nb_password/gen.py
@@ -0,0 +1,74 @@
+import csv, json
+
+file = "./data/crackingTimes.csv"
+
+data = {
+    'minimum': 0,
+    'maximum': 0,
+    'total': 0,
+    'data': 
+        {
+            'seconds': [],
+            'minutes': [],
+            'hours': [],
+            'days': [],
+            'months': [],
+            'years': []
+        }
+}
+with open(file) as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        lts = row[0].find('less than a second')
+        sec = row[0].find('second')
+        minute = row[0].find('minute')
+        hour = row[0].find('hour')
+        day = row[0].find('day')
+        month = row[0].find('month')
+        year = row[0].find('year')
+        
+        if(lts >= 0):
+            data['data']['seconds'].append({'t': 0, 'value': int(row[1])})
+        elif (sec >= 0):
+            data['data']['seconds'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+        elif (minute >= 0):
+            data['data']['minutes'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+        elif (hour >= 0):
+            data['data']['hours'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+        elif (day >= 0):
+            data['data']['days'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+        elif (month >= 0):
+            data['data']['months'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+        elif (year >= 0):
+            data['data']['years'].append({'t': int(row[0].split(' ')[0]), 'value': int(row[1])})
+
+# stats
+maximum = 0
+minimum = 10000000
+tot = 0
+for d in data['data']:
+    for e in data['data'][d]:
+        tot += e['value']
+        if e['value'] > maximum:
+            maximum = e['value']
+        if e['value'] < minimum:
+            minimum = e['value']
+
+# Append time 0 to every types
+tot = 0
+for t in data['data']:
+    for e in data['data'][t]:
+        tot += e['value']
+    if t != 'seconds':
+        data['data'][t].append({'t': 0, 'value': tot})
+
+#sort
+for d in data['data']:
+    data['data'][d] = sorted(data['data'][d], key=lambda k: k.get('t', 0), reverse=False)
+
+data['minimum'] = minimum
+data['maximum'] = maximum
+data['total'] = tot
+
+with open(f'data.json', 'w') as outfile:  
+    json.dump(data, outfile)
