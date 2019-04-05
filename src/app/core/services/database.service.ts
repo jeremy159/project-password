728e1119caa2d99459466971141e9b651fcc5a22
Jeremy Dube <dube.jeremy96@gmail.com>
2019-04-04 21:49:25 -0400

    Petites modif sur custom-password

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import { User } from 'src/app/shared/models/user';
-import { RestAPIService } from './restAPI.service';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
